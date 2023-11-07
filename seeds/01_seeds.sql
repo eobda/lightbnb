22a47d8f166bda291961da72ac821c7ed93f2eeb
maddie <lee.maddie@gmail.com>
2023-11-07 09:31:25 -0500

    Fix date entries

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -9,9 +9,9 @@ VALUES (1, 'Dawn Winery', 'description', 'https://images.pexels.com/photos/94370
 (3, 'Goth Grand Hotel', 'description', 'https://images.pexels.com/photos/5122670/pexels-photo-5122670.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/5122670/pexels-photo-5122670.jpeg', 500, 24, 14, 25, 'Canada', '1 Rideau St', 'Ottawa', 'Ontario', 'K1N 8S7', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES (2020-09-28, 2020-10-01, 1, 2),
-(2019-01-04, 2019-02-01, 2, 1),
-(2023-10-01, 2023-10-14, 2, 2);
+VALUES ('2020-09-28', '2020-10-01', 1, 2),
+('2019-01-04', '2019-02-01', 2, 1),
+('2023-10-01', '2023-10-14', 2, 2);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES (2, 1, 1, 5, 'message'),
