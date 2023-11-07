14eacbba803d0bcf59ab32d664af964254223e94
maddie <lee.maddie@gmail.com>
2023-11-07 16:01:47 -0500

    Add average rating

--- 1_queries/03_property_listings_by_city.sql
+++ 1_queries/03_property_listings_by_city.sql
@@ -1,5 +1,8 @@
-SELECT id, title, cost_per_night
+SELECT properties.id, title, cost_per_night, AVG(rating) AS average_rating
 FROM properties
+JOIN property_reviews ON properties.id = property_id
 WHERE city LIKE '%Vancouver'
+GROUP BY properties.id
+HAVING AVG(rating) >= 4
 ORDER BY cost_per_night
 LIMIT 10;
