a6d50a9ea411ad2d33b4cfbf0eeee5f742ea201b
maddie <lee.maddie@gmail.com>
2023-11-08 11:25:26 -0500

    Fix order by

--- 1_queries/04_most_visited_cities.sql
+++ 1_queries/04_most_visited_cities.sql
@@ -2,4 +2,4 @@ SELECT properties.city, COUNT(reservations.*) AS total_reservations
 FROM reservations
 JOIN properties ON properties.id = property_id
 GROUP BY properties.city
-ORDER BY total_reservations;
+ORDER BY total_reservations DESC;
