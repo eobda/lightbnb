03223ccd6988aac72be233a4ffb1d762e661df33
maddie <lee.maddie@gmail.com>
2023-11-08 12:22:06 -0500

    Change string to integer

--- 1_queries/05_all_my_reservations.sql
+++ 1_queries/05_all_my_reservations.sql
@@ -2,7 +2,7 @@ SELECT reservations.id, properties.title, properties.cost_per_night, reservation
 FROM reservations
 JOIN properties ON properties.id = reservations.property_id
 JOIN property_reviews ON properties.id = property_reviews.property_id
-WHERE reservations.guest_id = '4'
+WHERE reservations.guest_id = 4
 GROUP BY reservations.id, properties.title, properties.cost_per_night
 ORDER BY reservations.start_date
 LIMIT 10;
