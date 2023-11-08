c1561df3961c913cee3b7c9c04e0b197e5c04222
maddie <lee.maddie@gmail.com>
2023-11-08 12:23:01 -0500

    Fix group by syntax

--- 1_queries/05_all_my_reservations.sql
+++ 1_queries/05_all_my_reservations.sql
@@ -3,6 +3,6 @@ FROM reservations
 JOIN properties ON properties.id = reservations.property_id
 JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE reservations.guest_id = 4
-GROUP BY reservations.id, properties.title, properties.cost_per_night
+GROUP BY reservations.id, properties.id
 ORDER BY reservations.start_date
 LIMIT 10;
