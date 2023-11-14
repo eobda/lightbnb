08550739f87a429765f59d066f9257aec4c707f5
maddie <lee.maddie@gmail.com>
2023-11-14 12:22:51 -0500

    Update query string in getAllProperties to be filtered

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -116,8 +116,13 @@ const getAllReservations = function (guest_id, limit = 10) {
  */
 const getAllProperties = function (options, limit = 10) {
   const queryString = `
-    SELECT *
+    SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) AS average_rating
     FROM properties
+    LEFT JOIN property_reviews ON properties.id = property_id
+    WHERE city LIKE '%ancouv%'
+    GROUP BY properties.id
+    HAVING AVG(property_reviews.rating) >= 4
+    ORDER BY cost_per_night
     LIMIT $1;
   `;
   const values = [limit];
